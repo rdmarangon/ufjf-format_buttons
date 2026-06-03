cdaedfe65052b9b49f38cbd409337a601e366021
Rodrigo Marangon <r.marangon@gmail.com>
2026-06-03 15:31:25 -0300

    first commit

---
+++ lang/pt_br/format_buttons.php
@@ -0,0 +1,97 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     format_buttons
+ * @category    string
+ * @copyright   2023 Jhon Rangel <jrangelardila@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['privacy:metadata'] = 'Este plugin não armazena nenhum dado de usuário';
+$string['addsections'] = 'Adicionar seção';
+$string['currentsection'] = 'Esta seção';
+$string['deletesection'] = 'Excluir seção';
+$string['editsection'] = 'Editar seção';
+$string['editsectionname'] = 'Editar nome da seção';
+$string['hidefromothers'] = 'Ocultar seção';
+$string['mycustomstring'] = 'Você pode adicionar strings \'extras\' pelo arquivo de receitas.';
+$string['mycustomstring2'] = 'Outra string com o marcador {$a->some}.';
+$string['newsectionname'] = 'Novo nome para a seção {$a}';
+$string['pluginname'] = 'Formato Botões';
+$string['sectionname'] = 'Seção';
+$string['showfromothers'] = 'Mostrar seção';
+$string['cachedef_user_last_section'] = 'Cache da última seção do usuário';
+$string['fontcolor'] = 'Cor da fonte';
+$string['fontcolor_desc'] = 'Informe a cor da fonte para os botões';
+$string['config'] = 'Configuração do plugin';
+$string['bgcolor'] = 'Cor do botão';
+$string['bgcolor_desc'] = 'Informe a cor de fundo para os botões';
+$string['option1'] = 'Números';
+$string['option2'] = 'Alfabeto minúsculo';
+$string['option3'] = 'Alfabeto maiúsculo';
+$string['option4'] = 'Numerais romanos';
+$string['numeretion'] = 'Numeração';
+$string['numeretion_desc'] = 'Numeração dos botões';
+$string['settings'] = 'Configurações padrão';
+$string['colorfont'] = 'Informe a cor da fonte';
+$string['colorfont_help'] = 'Informe a cor do texto dos botões. Exemplo: #FFFFFF';
+$string['bgcolor_help'] = 'Informe a cor dos botões. Exemplo: #920020';
+$string['selectoption'] = 'Selecionar numeração dos botões';
+$string['selectoption_help'] = 'Indique a forma de numerar os botões';
+$string['bgcolor_selected'] = 'Cor do botão selecionado';
+$string['bgcolor_selected_desc'] = 'Fundo para o botão selecionado';
+$string['fontcolor_selected'] = 'Cor da fonte do botão selecionado';
+$string['fontcolor_selected_desc'] = 'Cor da fonte do botão selecionado';
+$string['selectd_file'] = 'Imagem para as seções';
+$string['selectd_file_desc'] = 'Imagem para o início das seções, no topo';
+$string['rounded'] = 'Arredondados';
+$string['square'] = 'Quadrados';
+$string['selectform'] = 'Indique o formato dos botões';
+$string['selectform_help'] = 'Selecione o formato em que os botões são exibidos';
+$string['title_section_view'] = 'Exibir o título de cada seção';
+$string['title_section_view_help'] = 'Indique se deve ser exibido o título de cada seção';
+$string['plugin_description'] = 'Botões é um formato de curso que cria um menu com botões para acessar as seções, uma por uma.';
+$string['section_zero_ubication'] = 'Exibir a seção 0 como botão';
+$string['section_zero_ubication_help'] = 'Indique se deseja que a seção 0 seja exibida como botão. Caso contrário, ficará no topo';
+$string['zero'] = 'Zero';
+$string['one'] = 'Um';
+$string['two'] = 'Dois';
+$string['three'] = 'Três';
+$string['four'] = 'Quatro';
+$string['five'] = 'Cinco';
+$string['six'] = 'Seis';
+$string['seven'] = 'Sete';
+$string['eight'] = 'Oito';
+$string['nine'] = 'Nove';
+$string['ten'] = 'Dez';
+$string['eleven'] = 'Onze';
+$string['twelve'] = 'Doze';
+$string['thirteen'] = 'Treze';
+$string['fourteen'] = 'Quatorze';
+$string['fifteen'] = 'Quinze';
+$string['sixteen'] = 'Dezesseis';
+$string['groups_course'] = 'Máximo de grupos';
+$string['groups_course_desc'] = 'Indique o número máximo de grupos que um curso pode ter';
+$string['title_gruping'] = 'Título do agrupamento {$a}';
+$string['color_gruping'] = 'Cor para os botões do grupo {$a}';
+$string['title_gruping_help'] = 'O texto é exibido antes do grupo';
+$string['sections_gruping'] = 'Número de seções para o grupo {$a}';
+$string['sections_gruping_help'] = 'Número de seções que o grupo contém';
